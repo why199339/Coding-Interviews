77ad153f2b29df04431e8fe16bc31454138d5fa8
why <wanghongyang@ict.ac.cn>
2016-08-08 16:31:24 +0800

    2016.8.8

---
+++ src/q24/VerifySequenceOfBST.java
@@ -0,0 +1,41 @@
+package q24;
+
+public class VerifySequenceOfBST {
+
+	public static void main(String[] args) {
+		int[] array1 = {5,7,6,9,11,10,8};
+		int[] array2 = {7,4,6,5};
+		System.out.println(verifySequenceOfBST(array1, 0, array1.length-1));
+		System.out.println(verifySequenceOfBST(array2, 0, array2.length-1));
+	}
+
+	public static boolean verifySequenceOfBST(int[] sequence, int start, int end) {
+		if (sequence == null) {
+			return false;
+		}
+		int root = sequence[end];
+		int i = start;
+		while (i < end) {
+			if (sequence[i] > root) {
+				break;
+			}
+			i++;
+		}
+		int j = i;
+		while (j < end) {
+			if (sequence[j] < root) {
+				return false;
+			}
+			j++;
+		}
+		boolean left = true;
+		if (i > start) {
+			left = verifySequenceOfBST(sequence, start, i - 1);
+		}
+		boolean right = true;
+		if (i < end) {
+			right = verifySequenceOfBST(sequence, start, end - 1);
+		}
+		return left && right;
+	}
+}
---
+++ src/q25/FindPath.java
@@ -0,0 +1,15 @@
+package q25;
+
+public class FindPath {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void findPath(BinaryTreeNode root, int sum) {
+		if(root == null) {
+			return;
+		}
+		
+	}
+}
